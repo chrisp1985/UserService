dbd8fc99bef63e003dfcd5eb6424d333509a7a8d
unknown <chriseparsons@hotmail.co.uk>
2024-10-28 21:32:23 +0000

    Added variables to github workflow, appplication yml etc.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,3 +4,25 @@
 
 
 ## Create App Runner
+resource "aws_apprunner_service" "example" {
+  service_name = "aws_app_runner_service_" + var.unique_id
+
+  source_configuration {
+    image_repository {
+      image_configuration {
+        port = "8000"
+        runtime_environment_variables = {
+          "CLUSTER_API_KEY" = var.cloud_cluster_api_key
+          "CLUSTER_API_SECRET" = var.cloud_cluster_api_secret
+        }
+      }
+      image_identifier      = "195571588534.dkr.ecr.eu-west-2.amazonaws.com/chrisp1985_ecr_docker_repo:f5983e69ce6e90f80a01a60cffb57051a27b5c7d"
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = true
+  }
+
+  tags = {
+    Name = "user-service-apprunner-service"
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,17 @@
+variable "cloud_cluster_api_key" {
+  description = "Confluent Cloud API Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "cloud_cluster_api_secret" {
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "unique_id" {
+  description = "Unique Id from Github sha."
+  type        = string
+  sensitive   = true
+}
